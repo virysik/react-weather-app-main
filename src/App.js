07a25e09735354f94414830d00a01d8fd2d0e739
virysik <virysik@gmail.com>
2020-10-08 12:12:35 +0200

    added GitHub URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <footer>
+        This project is created by Vera Kuzjo and is an
+        <a
+          href="https://github.com/virysik/react-weather-app-main"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
