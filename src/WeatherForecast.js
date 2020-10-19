180e057860546c1ecb83a63e0289248b1828103f
virysik <68701496+virysik@users.noreply.github.com>
2020-10-19 09:50:12 +0200

    took away one state in forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,26 +5,27 @@ import WeatherForecastPreview from "./WeatherForecastPreview";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
-    const [ready, setReady] = useState(false);
-    const [forecast, setForecast] = useState(null);
+    const [forecast, setForecast] = useState({ready: false});
 
 function handleForecast(response) { 
-    setForecast(response.data);
-    setReady(true);
+    setForecast({
+        element: response.data,
+        ready: true
+    });
 }
 
-    if(ready && props.lat === forecast.lat) {
+    if(forecast.ready && props.lat === forecast.element.lat) {
     return (
         <div className = "WeatherForecast row">
-            <WeatherForecastPreview data = {forecast.daily[1]}/>
-            <WeatherForecastPreview data = {forecast.daily[2]}/>
-            <WeatherForecastPreview data = {forecast.daily[3]}/>
-            <WeatherForecastPreview data = {forecast.daily[4]}/>
-            <WeatherForecastPreview data = {forecast.daily[5]}/>
+            <WeatherForecastPreview data = {forecast.element.daily[1]}/>
+            <WeatherForecastPreview data = {forecast.element.daily[2]}/>
+            <WeatherForecastPreview data = {forecast.element.daily[3]}/>
+            <WeatherForecastPreview data = {forecast.element.daily[4]}/>
+            <WeatherForecastPreview data = {forecast.element.daily[5]}/>
          </div>
     );
     } else {
-        const apiKey = "974437790c20752769b5d2ac36ae13ef";
+        const apiKey = "b809e096c73a6d18c5b757c80921ea71";
         const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.lat}1&lon=${props.lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=metric`;
         axios.get(url).then(handleForecast);
         return (
